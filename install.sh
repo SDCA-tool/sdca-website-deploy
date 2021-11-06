e0c7c4055b7a035431b8153b155618afc22287e6
martin <martin@lucas-smith.co.uk>
2021-11-06 19:20:41 +0000

    Ensure install script runs as root

--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
-# Installs the system
+# Installs the system software
+# Written for Ubuntu Server 20.04 LTS
+# This script is idempotent - it can be safely re-run without destroying existing data
+
+
+# Ensure this script is run as root
+if [ "$(id -u)" != "0" ]; then
+    echo "# This script must be run as root." 1>&2
+    exit 1
+fi
 
 
 # Update packages index
-sudo apt-get update
+apt-get update
 
 # Webserver
-sudo apt-get -y install apache2
+apt-get -y install apache2
+
+
 
 
 # Patch system
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
-sudo apt-get -y autoremove
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y autoremove
