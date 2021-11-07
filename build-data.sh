e2be1fb64a0d17eb79206b6ab65810e2c91ba530
mvl22 <martin@lucas-smith.co.uk>
2021-11-07 23:44:55 +0000

    Rename layer in build script

--- build-data.sh
+++ build-data.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# Builds data for the site
+# Builds vector tile data for the site
+# See tutorial at: https://github.com/ITSLeeds/VectorTiles
+# See examples at: https://github.com/creds2/CarbonCalculator/blob/master/tippercanoe
 
 
 # Do work in /tmp/
@@ -9,11 +11,11 @@ cd /tmp/
 # Specify output data folder
 OUTPUT=/var/www/sdca/data/
 
-# Bus stops
+# Public transport stops
 wget https://github.com/creds2/CarbonCalculator/releases/download/1.0/PBCC_transit_stop_frequency_2020.zip
 unzip PBCC_transit_stop_frequency_2020.zip
 rm PBCC_transit_stop_frequency_2020.zip
-tippecanoe --output-to-directory=transitstops --name=transitstops --layer=transitstops --attribution=MALCOLMMORGAN --maximum-zoom=13 --minimum-zoom=4  --drop-densest-as-needed -rg4 --force  transit_stop_frequency_v3.geojson
+tippecanoe --output-to-directory=publictransport --name=publictransport --layer=publictransport --attribution=MALCOLMMORGAN --maximum-zoom=13 --minimum-zoom=4 --drop-densest-as-needed -rg4 --force  transit_stop_frequency_v3.geojson
 rm transit_stop_frequency_v3.geojson
-rm -rf "${OUTPUT}/transitstops/"
-mv transitstops "${OUTPUT}/"
+rm -rf "${OUTPUT}/publictransport/"
+mv publictransport "${OUTPUT}/"
