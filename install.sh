0ac3033ee96e9e2ea9dee0e5438da8daee06b463
mvl22 <martin@lucas-smith.co.uk>
2021-11-21 17:42:15 +0000

    Add data repo

--- install.sh
+++ install.sh
@@ -86,6 +86,13 @@ if [ ! -d /var/www/sdca/sdca-website/ ]; then
 	chown -R sdca.rollout /var/www/sdca/sdca-website/ && chmod -R g+ws /var/www/sdca/sdca-website/
 fi
 
+# Add data repo
+if [ ! -d /var/www/sdca/sdca-data/ ]; then
+	cd /var/www/sdca/
+	git clone https://github.com/SDCA-tool/sdca-data.git
+	chown -R sdca.rollout /var/www/sdca/sdca-data/ && chmod -R g+ws /var/www/sdca/sdca-data/
+fi
+
 # Add library repo
 if [ ! -d /var/www/sdca/Mapboxgljs.LayerViewer/ ]; then
 	cd /var/www/sdca/
