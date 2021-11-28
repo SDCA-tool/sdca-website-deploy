454eadabc1be807cd027b364539c5214d2ef0d22
mvl22 <martin@lucas-smith.co.uk>
2021-11-28 16:20:48 +0000

    Remove old manual transit stops processing

--- build-data.sh
+++ build-data.sh
@@ -54,15 +54,3 @@ csvtool namedcol id,zipfile,title,description,has_attributes,source,source_url,t
 	
 done
 
-
-exit
-
-
-# Public transport stops
-wget https://github.com/creds2/CarbonCalculator/releases/download/1.0/PBCC_transit_stop_frequency_2020.zip
-unzip PBCC_transit_stop_frequency_2020.zip
-rm PBCC_transit_stop_frequency_2020.zip
-tippecanoe --output-to-directory=publictransport --name=publictransport --layer=publictransport --attribution=MALCOLMMORGAN --maximum-zoom=13 --minimum-zoom=4 --drop-densest-as-needed -rg4 --force  transit_stop_frequency_v3.geojson
-rm transit_stop_frequency_v3.geojson
-rm -rf "${OUTPUT}/publictransport/"
-mv publictransport "${OUTPUT}/"
