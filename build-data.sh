6eced0a341a03d6dd653825ef8cbe4dfe1e62098
mvl22 <martin@lucas-smith.co.uk>
2021-11-29 00:37:03 +0000

    Bring in geometries_type field in sync with https://github.com/SDCA-tool/sdca-data/commit/d25e6bf8664ee09812b044469d4ae7e8f183b531

--- build-data.sh
+++ build-data.sh
@@ -26,9 +26,9 @@ OUTPUT=/var/www/sdca/data/
 
 # Loop through datasets; see: https://unix.stackexchange.com/a/622269/168900
 # Data at: https://github.com/SDCA-tool/sdca-data/releases
-csvtool namedcol id,zipfile,title,description,has_attributes,source,source_url,tippecanoeparams $dataRepo/datasets.csv \
+csvtool namedcol id,zipfile,title,description,geometries_type,has_attributes,source,source_url,tippecanoeparams $dataRepo/datasets.csv \
  | csvtool -u '|' drop 1 - \
- | while IFS=$'|' read -r id zipfile title description has_attributes source source_url tippecanoeparams; do
+ | while IFS=$'|' read -r id zipfile title description geometries_type has_attributes source source_url tippecanoeparams; do
 	
 	echo -e "\n\nProcessing dataset ${id}:\n"
 
