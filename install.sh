e881e4c7085f9249e2ad5c7c0a6ebedfda139051
mvl22 <martin@lucas-smith.co.uk>
2022-02-13 19:47:44 +0000

    Move Munin Node install to end

--- install.sh
+++ install.sh
@@ -52,16 +52,6 @@ if ! which tippecanoe >/dev/null; then
 	cd "${DIR}/"
 fi
 
-# Munin Node, which should be installed after all other software; see: https://www.digitalocean.com/community/tutorials/how-to-install-the-munin-monitoring-tool-on-ubuntu-14-04
-# Include dependencies for Munin MySQL plugins; see: https://raymii.org/s/snippets/Munin-Fix-MySQL-Plugin-on-Ubuntu-12.04.html
-apt-get install -y libcache-perl libcache-cache-perl
-# Add libdbi-perl as otherwise /usr/share/munin/plugins/mysql_ suggest will show missing DBI.pm; see: http://stackoverflow.com/questions/20568836/cant-locate-dbi-pm and https://github.com/munin-monitoring/munin/issues/713
-apt-get install -y libdbi-perl libdbd-mysql-perl
-apt-get install -y munin-node
-apt-get install -y munin-plugins-extra
-munin-node-configure --suggest --shell | sh
-service munin-node restart
-
 # Add firewall
 # https://help.ubuntu.com/community/UFW
 # Check status using: sudo ufw status verbose
@@ -207,6 +197,16 @@ fi
 # Add locate
 apt-get install -y locate
 updatedb
+
+# Munin Node, which should be installed after all other software; see: https://www.digitalocean.com/community/tutorials/how-to-install-the-munin-monitoring-tool-on-ubuntu-14-04
+# Include dependencies for Munin MySQL plugins; see: https://raymii.org/s/snippets/Munin-Fix-MySQL-Plugin-on-Ubuntu-12.04.html
+# Add libdbi-perl as otherwise /usr/share/munin/plugins/mysql_ suggest will show missing DBI.pm; see: http://stackoverflow.com/questions/20568836/cant-locate-dbi-pm and https://github.com/munin-monitoring/munin/issues/713
+apt-get install -y libcache-perl libcache-cache-perl
+apt-get install -y libdbi-perl libdbd-mysql-perl
+apt-get install -y munin-node
+apt-get install -y munin-plugins-extra
+munin-node-configure --suggest --shell | sh
+service munin-node restart
 
 # Build data
 su - sdca "${DIR}/build-data.sh"
